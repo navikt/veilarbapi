e34503331c003c8bb1d76477ff6802572b430142
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-01-31 15:34:11 +0100

    Initial commit

---
+++ src/main/kotlin/no/nav/poao/Application.kt
@@ -0,0 +1,13 @@
+package no.nav.poao
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import no.nav.poao.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureMonitoring()
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/no/nav/poao/plugins/Monitoring.kt
@@ -0,0 +1,10 @@
+package no.nav.poao.plugins
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureMonitoring() {
+
+
+}
