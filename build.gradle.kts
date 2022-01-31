1c9e019ed2412d3baec37ff9364aa37632ac098f
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-01-31 16:26:03 +0100

    Bygge klientkode

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -6,6 +8,7 @@ plugins {
     application
     kotlin("jvm") version "1.6.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    id ("org.openapi.generator") version "5.3.0"
 }
 
 group = "no.nav.poao"
@@ -16,6 +19,16 @@ application {
 
 repositories {
     mavenCentral()
+}
+
+openApiGenerate {
+    generatorName.set("kotlin")
+    inputSpec.set("$projectDir/src/main/resources/openapi/ArbeidsoppfolgingV1.yaml")
+    outputDir.set("$buildDir/generated")
+}
+
+tasks.named( "compileKotlin") {
+    dependsOn( ":openApiGenerate" )
 }
 
 dependencies {
