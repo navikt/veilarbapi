b7dcc62d0b1b4d0f508cbdee518df2777883f49f
haakonph <hakon.planke.holm@nav.no>
2022-02-03 16:02:20 +0100

    fiks serialisering

--- src/main/kotlin/no/nav/poao/plugins/Routing.kt
+++ src/main/kotlin/no/nav/poao/plugins/Routing.kt
@@ -3,21 +3,18 @@ package no.nav.poao.plugins
 import io.ktor.routing.*
 import io.ktor.application.*
 import io.ktor.response.*
+import no.nav.veilarbapi.JSON
 import no.nav.veilarbapi.model.Aktivitet
 import no.nav.veilarbapi.model.Mote
 import no.nav.veilarbapi.model.Oppfolgingsperiode
 
 fun Application.configureRouting() {
-
-
-
     routing {
         get("/arbeidsoppfolging") {
-
             val oppfolgingsperiode = Oppfolgingsperiode()
             val mote = Mote()
-            oppfolgingsperiode.addAktiviteterItem(Aktivitet( mote))
-            oppfolgingsperiode
+            val aktivitet = Aktivitet(mote)
+            oppfolgingsperiode.addAktiviteterItem(aktivitet)
             call.respond(oppfolgingsperiode)
         }
     }
--- src/main/kotlin/no/nav/poao/plugins/Serialization.kt
+++ src/main/kotlin/no/nav/poao/plugins/Serialization.kt
@@ -1,24 +1,15 @@
 package no.nav.poao.plugins
 
-import io.ktor.serialization.*
 import io.ktor.features.*
 import io.ktor.application.*
 import io.ktor.gson.*
 import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import no.nav.veilarbapi.JSON
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        gson(block = {JSON.createGson()})
-    }
-
-
-    routing {
-        get("/gson/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
+        JSON()
+        val converter = GsonConverter(JSON.getGson())
+        register(ContentType.Application.Json, converter)
     }
 }
