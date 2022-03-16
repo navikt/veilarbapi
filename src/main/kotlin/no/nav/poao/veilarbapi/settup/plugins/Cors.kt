ee795f892feafdee13f4df61f6c434f7980e16da
marcjas <marc.joseph.ablian.saputil@nav.no>
2022-03-16 12:03:13 +0100

    test

--- src/main/kotlin/no/nav/poao/veilarbapi/settup/plugins/Cors.kt
+++ src/main/kotlin/no/nav/poao/veilarbapi/settup/plugins/Cors.kt
@@ -2,10 +2,12 @@ package no.nav.poao.veilarbapi.settup.plugins
 
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.http.*
 
 fun Application.configureCors() {
     install(CORS) {
         host("localhost:63342")
+        method(HttpMethod.Options)
         allowCredentials = true
     }
 }
