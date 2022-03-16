67b76ea424a9f22ba17938a2e18efb9825fee8df
marcjas <marc.joseph.ablian.saputil@nav.no>
2022-03-16 12:44:27 +0100

    test

--- src/main/kotlin/no/nav/poao/veilarbapi/settup/plugins/Cors.kt
+++ src/main/kotlin/no/nav/poao/veilarbapi/settup/plugins/Cors.kt
@@ -7,6 +7,7 @@ import io.ktor.http.*
 fun Application.configureCors() {
     install(CORS) {
         host("localhost:63342")
+        host("https://navikt.github.io/veilarbapi")
         method(HttpMethod.Options)
         allowCredentials = true
     }
