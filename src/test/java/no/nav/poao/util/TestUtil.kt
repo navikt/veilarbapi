53ee8be2f855beb2706278367b9ca067b9449772
marcjas <marc.joseph.ablian.saputil@nav.no>
2022-04-27 15:43:47 +0200

    slette ubrukt kode

--- src/test/java/no/nav/poao/util/TestUtil.kt
+++ src/test/java/no/nav/poao/util/TestUtil.kt
@@ -3,15 +3,7 @@ package no.nav.poao.util
 import io.ktor.client.*
 import io.ktor.client.engine.mock.*
 import io.ktor.client.request.*
-import io.ktor.server.testing.*
 
-//internal fun createMockClient(block: HttpClientConfig<MockEngineConfig>.() -> Unit = {}): HttpClient {
-//    return HttpClient(MockEngine) {
-//        expectSuccess = false
-//
-//        this.apply(block)
-//    }
-//}
 
 internal fun createMockClient(block: MockRequestHandleScope.(HttpRequestData) -> HttpResponseData): HttpClient {
     return HttpClient(MockEngine) {
