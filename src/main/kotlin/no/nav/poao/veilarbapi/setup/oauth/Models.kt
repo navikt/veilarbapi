b5519856571d7f2ff8a1a0dcef1d82b3bf354103
Marc <rhyor420@gmail.com>
2022-05-03 13:32:48 +0200

    slett ubrukt kode (#38)
    slett ubrukt oauth tokensupport-kode

--- src/main/kotlin/no/nav/poao/veilarbapi/setup/oauth/Models.kt
+++ src/main/kotlin/no/nav/poao/veilarbapi/setup/oauth/Models.kt
@@ -1,13 +1,5 @@
 package no.nav.poao.veilarbapi.setup.oauth
 
-data class Resource(
-    val clientId: String,
-    val url: String,
-    val response: Any? = null
-) {
-    fun addResponse(response: Any?): Resource = this.copy(response = response)
-}
-
 data class ThrowableErrorMessage(
     val message: String,
     val throwable: Throwable
