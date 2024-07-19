cc7c6a43beae5add42ef45f1744e7ba06c33d7a7
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-07-19 13:53:13 +0200

    Fjerne sonacube ci run - automatic scan enabled on sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
     id ("org.openapi.generator") version "5.4.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("org.sonarqube") version "3.3"
+    id("org.sonarqube") version "4.4.1.3373"
     id ("jacoco")
 }
 
@@ -28,9 +28,9 @@ sonarqube {
     }
 }
 
-tasks.sonarqube {
-    dependsOn(tasks.jacocoTestReport)
-}
+//tasks.sonarqube {
+//    dependsOn(tasks.jacocoTestReport)
+//}
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
