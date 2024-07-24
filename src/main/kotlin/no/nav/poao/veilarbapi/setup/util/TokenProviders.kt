9f1fe4ceb9085ea2690af166305ce7e01c610a9b
johannetronstad <johanne.tronstad@nav.no>
2024-07-24 13:04:39 +0200

    Kast exception tidligere

--- src/main/kotlin/no/nav/poao/veilarbapi/setup/util/TokenProviders.kt
+++ src/main/kotlin/no/nav/poao/veilarbapi/setup/util/TokenProviders.kt
@@ -2,6 +2,7 @@ package no.nav.poao.veilarbapi.setup.util
 
 import com.github.michaelbull.result.get
 import no.nav.poao.veilarbapi.setup.config.Configuration
+import no.nav.poao.veilarbapi.setup.exceptions.IkkePaaLoggetException
 import no.nav.poao.veilarbapi.setup.oauth.AzureAdClient
 
 class TokenProviders(
@@ -9,31 +10,25 @@ class TokenProviders(
     val configuration: Configuration
 ) {
 
-    val veilarbaktivitetTokenProvider: suspend (String?) -> String? = { accessToken ->
-        accessToken?.let {
-            azureAdClient.getOnBehalfOfAccessTokenForResource(
-                scopes = listOf(configuration.veilarbaktivitetConfig.authenticationScope),
-                accessToken = it
-            ).get()?.accessToken
-        }
+    val veilarbaktivitetTokenProvider: suspend (String) -> String = { accessToken ->
+        azureAdClient.getOnBehalfOfAccessTokenForResource(
+            scopes = listOf(configuration.veilarbaktivitetConfig.authenticationScope),
+            accessToken = accessToken
+        ).let { it.get()?.accessToken ?: throw it.component2()!!.throwable }
     }
 
-    val veilarbdialogTokenProvider: suspend (String?) -> String? = { accessToken ->
-        accessToken?.let {
-            azureAdClient.getOnBehalfOfAccessTokenForResource(
-                scopes = listOf(configuration.veilarbdialogConfig.authenticationScope),
-                accessToken = it
-            ).get()?.accessToken
-        }
+    val veilarbdialogTokenProvider: suspend (String) -> String = { accessToken ->
+        azureAdClient.getOnBehalfOfAccessTokenForResource(
+            scopes = listOf(configuration.veilarbdialogConfig.authenticationScope),
+            accessToken = accessToken
+        ).let { it.get()?.accessToken ?: throw it.component2()!!.throwable }
     }
 
-    val veilarboppfolgingTokenProvider: suspend (String?) -> String? = { accessToken ->
-        accessToken?.let {
-            azureAdClient.getOnBehalfOfAccessTokenForResource(
-                scopes = listOf(configuration.veilarboppfolgingConfig.authenticationScope),
-                accessToken = it
-            ).get()?.accessToken
-        }
+    val veilarboppfolgingTokenProvider: suspend (String) -> String = { accessToken ->
+        azureAdClient.getOnBehalfOfAccessTokenForResource(
+            scopes = listOf(configuration.veilarboppfolgingConfig.authenticationScope),
+            accessToken = accessToken
+        ).let { it.get()?.accessToken ?: throw it.component2()!!.throwable }
     }
 
 }
